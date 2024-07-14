ef7542a52a16850ee92d70f81ddbe4740e75e386
shih <shih@shihdebgdiannao.lan>
2024-07-14 02:31:28 -0700

    change logout router

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
       </ul>
       <div className='nav-login-cart'>
         {localStorage.getItem('auth-token')
-        ?<button onClick={ ()=>{localStorage.removeItem('auth-token'); window.location.replace('/')}}>Logout</button>
+        ?<button onClick={ ()=>{localStorage.removeItem('auth-token'); window.location.replace('/DOGFD')}}>Logout</button>
         :<Link to='/login'><button>Login</button></Link>
         }
         <Link to='/cart'><img src={cart_icon} alt="" /></Link>
